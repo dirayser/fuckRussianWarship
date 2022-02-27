765b6259fe3e1f88089dcb8650f33bd1558049dd
dirayser <dirayser@gmail.com>
2022-02-28 00:07:53 +0200

    fix

--- bot.js
+++ bot.js
@@ -10,9 +10,8 @@ const PORT = process.env.PORT;
 
 const bot = new Telegraf(BOT_TOKEN);
 
-bot.telegram.setWebhook(`${BOT_URL}/bot${TOKEN}`);
-bot.startWebhook(`/bot${TOKEN}`, null, PORT);
-
+bot.telegram.setWebhook(`${BOT_URL}/bot${BOT_TOKEN}`);
+bot.startWebhook(`/bot${BOT_TOKEN}`, null, PORT);
 
 bot.start(ctx => ctx.reply('РУССКИЙ ВОЕННЫЙ КОРАБЛЬ, ИДИ НАХУЙ!'));
 
