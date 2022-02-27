0ba5b7bf69e289b71c11cb730bc783f084089fe7
dirayser <dirayser@gmail.com>
2022-02-27 23:00:45 +0200

    init

---
+++ bot.js
@@ -0,0 +1,33 @@
+'use strict';
+
+require('dotenv');
+
+const Telegraf = require('telegraf');
+const express = require('express');
+
+const app = express();
+const BOT_TOKEN = process.env.BOT_TOKEN;
+const BOT_URL = process.env.BOT_TOKEN;
+const PORT = process.env.PORT;
+
+const bot = new Telegraf(BOT_TOKENN);
+
+bot.telegram.setWebhook(`${BOT_URL}/bot${BOT_TOKEN}`);
+app.use(bot.webhookCallback(`/bot${BOT_TOKEN}`));
+
+bot.on('text', ctx => {
+    const text = ctx.message.text;
+    if(text.toUpperCase() == 'РУССКИЙ' || text.toUpperCase() == 'РУССКІЙ') {
+        bot.telegram.sendMessage(ctx.message.chat.id, 'корабль, ИДИ НАХУЙ!', {
+          reply_to_message_id: ctx.message.message_id
+        });
+    } else if(text.toUpperCase() == 'РУССКИЙ КОРАБЛЬ' || text.toUpperCase() == 'РУССКІЙ КОРАБЛЬ') {
+        bot.telegram.sendMessage(ctx.message.chat.id, 'ИДИ НАХУЙ!', {
+          reply_to_message_id: ctx.message.message_id
+        });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
